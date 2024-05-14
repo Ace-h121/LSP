806a461c5f88fdb6aa169680a78bd2fbfea9355f
Ace-h121 <chathaway121@gmail.com>
2024-05-14 12:11:19 -0400

    actual using decoded info! W/ tests of course

--- rpc/rpc_test.go
+++ rpc/rpc_test.go
@@ -1,20 +1,35 @@
 package rpc_test
 
 import (
-	
-	"Github.com/LSP/rpc"
 	"testing"
+
+	"Github.com/LSP/rpc"
 )
 
-
-type EncodingExample struct{
-	testing bool
+type EncodingExample struct {
+	Testing bool
 }
 
 func TestEncode(t *testing.T) {
-	expected := "Content-Length: 16\r\n\r\n{\"testing\":true}"
-	actual := rpc.EncodeMessage(EncodingExample{testing:true})
+	expected := "Content-Length: 16\r\n\r\n{\"Testing\":true}"
+	actual := rpc.EncodeMessage(EncodingExample{Testing: true})
 	if expected != actual {
 		t.Fatalf("Expected %s, got: %s", expected, actual)
+	}
+}
+
+func TestDecode(t *testing.T) {
+	expected := "Content-Length: 15\r\n\r\n{\"Method\":\"hi\"}"
+	method, contentLength, err := rpc.DecodeMessage([]byte(expected))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if contentLength != 15 {
+		t.Fatalf("Expected: 15 Got :%d", contentLength)
+	}
+
+	if method != "hi" {
+		t.Fatalf("Expected hi, got %s", method)
 	}
 }
